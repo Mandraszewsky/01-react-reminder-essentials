c3ef832da3bfe48516584135858812c25c700980
Mandraszewsky <mandrah@o2.pl>
2025-01-04 15:27:39 +0100

    reusable components (header & coreconcept) with props

---
+++ src/components/CoreConcept.jsx
@@ -0,0 +1,10 @@
+//also can be function CoreConcept({image, title, description})
+export default function CoreConcept(props) {
+    return (
+      <li>
+        <img src={props.image} alt={props.title} />
+        <h3>{props.title}</h3>
+        <p>{props.description}</p>
+      </li>
+    );
+  }
